a5e39b404a1ae1fd0745b8a7ce3c87e86b521b79
Jadwiga Przebinda <jadzia@maczek.local>
2021-07-14 10:19:21 +0200

    initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from 'gatsby';
+import { IdentityContextProvider } from "react-netlify-identity-widget";
+
+import './layout.css';
+
+const Layout = ({children}) => (
+    <IdentityContextProvider url="https://jamstack-intro-auth.netlify.com">
+        <header>
+            <Link to="/">Jamstack App</Link>
+        </header>
+        <main>
+            {children}
+        </main>
+    </IdentityContextProvider>
+)
+
+export default Layout;
